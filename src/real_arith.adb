0e80526eb8852630d668a887f314a01fc79a420b
Yannick Moy <moy@adacore.com>
2021-01-26 16:43:41 +0100

    Ajout de lemmes en exemple

--- src/real_arith.adb
+++ src/real_arith.adb
@@ -22,6 +22,43 @@ package body Real_Arith is
          end loop;
          return S;
       end Sum_Floating;
+
+      --  Si seuls les prouveurs CVC4 et Z3 sont utilisés, ils ne suffisent
+      --  pas à démontrer la postcondition de Sum. On peut alors chercher à
+      --  démontrer cette borne supérieure de manière extrinsèque à l'aide
+      --  du lemme Lemma_Sum_Upper_Bound, qui amène à définir le lemme
+      --  d'arithmétique pure Lemma_Bump_Small_Integral_Float qui peut être
+      --  soit testé exhaustivement, soit prouvé avec un assistant de preuve
+      --  comme Coq.
+
+      --  Notez que le prouveur Alt-Ergo prouve ici la postcondition de Sum et
+      --  ces lemmes, mais cet exemple est une illustration d'utilisation de la
+      --  programmation auto-active.
+
+      procedure Lemma_Bump_Small_Integral_Float (I : Natural)
+      with
+        Ghost,
+        Pre  => I < 2**24,
+        Post => Float (I) + 1.0 = Float (I + 1)
+      is
+      begin
+         null;
+      end Lemma_Bump_Small_Integral_Float;
+
+      procedure Lemma_Sum_Upper_Bound (A : Data; Up_To : Index)
+      with
+        Ghost,
+        Pre  => Up_To <= A'Last,
+        Post => Sum (A, Up_To) <= Float (Up_To) * Max_Value,
+        Subprogram_Variant => (Decreases => Up_To)
+      is
+      begin
+         if Up_To /= 0 then
+            Lemma_Sum_Upper_Bound (A, Up_To - 1);
+            Lemma_Bump_Small_Integral_Float (Up_To - 1);
+            pragma Assert (Float (Up_To) = Float (Up_To - 1) + Float (1));
+         end if;
+      end Lemma_Sum_Upper_Bound;
    end Floating;
 
 end Real_Arith;
